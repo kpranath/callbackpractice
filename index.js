e29af008dd08d25dd47b626a6e4cbe76938788d5
Keshava Pranath K <kpranath@gmail.com>
2020-06-30 07:26:22 +0530

    promises added and promise api in progress

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
+// console.log('Before');
+// getUser(1, getRepositories);
+// console.log('After');
+
 console.log('Before');
-getUser(1, getRepositories);
+getUser(1)
+    .then(user => getGihubRepositories(user.githubUsename))
+    .then(repos => getCommits(repos[0]))
+    .then(commits => console.log('commits : ', commits))
+    .catch(err => console.log('Error: ', err.message));
 console.log('After');
 
-function getRepositories(user) {
-    getGihubRepositories(user.githubUsename, getrepoCommits);
+
+
+// function getRepositories(user) {
+//     getGihubRepositories(user.githubUsename, getrepoCommits);
+// }
+
+// function getrepoCommits(repos) {
+//     getCommits(repos[0], displaycommits);
+// }
+
+// function displaycommits(commits) {
+//     console.log(commits);
+// }
+
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from database');
+            resolve({ id: id, githubUsename: 'kpranath' });
+        }, 2000);
+    });
 }
 
-function getrepoCommits(repos) {
-    getCommits(repos[0], displaycommits);
+function getGihubRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('calling GitHub API...');
+            resolve(['repo1', 'repo2', 'repo3']);
+        }, 2000);
+    });
 }
 
-function displaycommits(commits) {
-    console.log(commits);
-}
-
-function getUser(id, callback) {
-    setTimeout(() => {
-        console.log('Reading a user from database');
-        callback({ id: id, githubUsename: 'kpranath' });
-    }, 2000);
-}
-
-function getGihubRepositories(username, callback) {
-    setTimeout(() => {
-        console.log('calling GitHub API...');
-        callback(['repo1', 'repo2', 'repo3']);
-    }, 2000);
-}
-
-function getCommits(repo, callback) {
-    setTimeout(() => {
-        console.log('calling GitHub API for commits...');
-        callback(['commit1', 'commit2', 'commit3']);
-    }, 2000);
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('calling GitHub API for commits...');
+            resolve(['commit1', 'commit2', 'commit3']);
+        }, 2000);
+    });
 }
