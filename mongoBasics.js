49f28a80af519db2d56198e96214925195d95bfd
Keshava Pranath K <kpranath@gmail.com>
2020-07-13 21:56:43 +0530

    basic validation and structuring

--- mongoBasics.js
+++ mongoBasics.js
@@ -34,9 +34,53 @@ async function createCourse() {
 async function getCourse() {
     const courses = await Course
         .find({ author: 'Mosh' })
+
+        //for regular expresion
+        //for author starting with Pranath
+        // .find({ author: /^Pranath/})
+        //for  author ending with Pranath
+        // .find({ author: /Pranath$/})
+        //for author containing Pranath
+        // .find({ author: /.*Pranath.*/i})     //i to make the pattern case insensitive
+
+
+        // .find({price: {$gt: 10}})  //gt is operator. So key is operator and is specified with $. other comparisions (eq,ne,gt,gte,lt,lte,in,nin)
+        // .find({price: {$gt: 10, $lt:20}}) //for rows in range
+        // .find({ price: { $in: [10, 15, 20] } })
+
+
         .limit(10)
         .sort({ name: 1 })
         .select({ name: 1, tags: 1 });
+
+
+    // for logical operators in the querying... or and and are the logical operators
+    // const courses = await Course
+    // .find()
+    // .or([ {author: 'Mosh' },{isPublished: true } ])
+    // .limit(10)
+    // .sort({ name: 1 })
+    // .select({ name: 1, tags: 1 });
+
+
+    //for counting
+    // const courses = await Course
+    //     .find({ author: 'Mosh' })
+    //     .limit(10)
+    //     .sort({ name: 1 })
+    //     .count();
+
+    //for pagination
+    // const pageNumber = 2;
+    // const pageSize = 10;
+    // const courses = await Course
+    //     .find({ author: 'Mosh' })
+    //     .skip((pageNumer-1)*pageSize)
+    //     .limit(pageSize)
+    //     .sort({ name: 1 })
+    //     .count();
+
+
     console.log(courses);
 }
 
