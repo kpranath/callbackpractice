b2dec2858aeff5e8f93565d89fccaefedf79fd8f
Keshava Pranath K <kpranath@gmail.com>
2020-07-17 22:30:59 +0530

    some more thing

--- embedding.js
+++ embedding.js
@@ -14,16 +14,16 @@ const Author = mongoose.model('Author', authorSchema);
 
 const Course = mongoose.model('Course', new mongoose.Schema({
     name: String,
-    author: {
-        type: authorSchema,
+    authors: {
+        type: [authorSchema],
         required: true //or we can also make required in the author schema for the required property...
     }
 }));
 
-async function createCourse(name, author) {
+async function createCourse(name, authors) {
     const course = new Course({
         name,
-        author
+        authors
     });
 
     const result = await course.save();
@@ -48,5 +48,25 @@ async function updateAuthor(courseId) {
     console.log(course);
 }
 
-updateAuthor('5f1106415d301e2db09d793f');
-// createCourse('Node Course', new Author({ name: 'Pranath' }));
+async function addAuthor(courseId, author) {
+    const course = await Course.findById(courseId);
+    course.authors.push(author);
+    course.save();
+    console.log(course);
+}
+
+async function removeAuthor(courseId, authorId) {
+    const course = await Course.findById(courseId);
+    const author = course.authors.id(authorId);
+    author.remove();
+    course.save();
+    console.log(course);
+}
+
+// updateAuthor('5f1106415d301e2db09d793f');
+// createCourse('Node Course', [
+// new Author({ name: 'Pranath' }),
+// new Author({ name: 'Lakshya' })
+// ]);
+// addAuthor('5f11d61103d984382418dfc4', new Author({ name: 'Sidharth' }));
+removeAuthor('5f11d61103d984382418dfc4', '5f11d76fbc538519b8b390b4')
